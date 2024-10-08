671c6278f6a2b69621dc18f27f29a40df493d139
rahatutzaman-rizon <redwantamim525@gmail.com>
2024-10-08 12:32:56 +0600

    update terms and privacy page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,7 +4,7 @@ export default function DashboardIndexPage() {
   return (
     <section className="bg-white p-6 rounded-lg my-4">
       <h2 className="text-green-600 text-2xl font-bold text-center">
-        Welcome, JMC Technology ! <br /> Dashboard v-2
+        Welcome, JMC Technology ltd ! <br /> Dashboard v-2
       </h2>
     </section>
   );
